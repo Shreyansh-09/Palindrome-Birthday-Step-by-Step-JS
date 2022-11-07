38597aff43355ec186da4d59760b5184b6c1c971
Shreyansh-09 <2509shreyansh@gmail.com>
2022-11-07 23:52:25 +0530

    ex-03: convert-date-to-string

--- app.js
+++ app.js
@@ -19,3 +19,28 @@ function checkPalindrome(string){
         return(false);
     }
 }
+
+function convertDateFromNumberToString(number){
+    var dateInString = {day: '', month: '', year: ''};
+    
+    var day = number.day;
+    var month = number.month;
+    var year = number.year;
+
+    if(day < 10){
+        dateInString.day = '0' + day;
+    }
+    else{
+        dateInString.day = day.toString();
+    }
+
+    if(month < 10){
+        dateInString.month = '0' + month;
+    }
+    else{
+        dateInString.month = month.toString();
+    }
+    dateInString.year = year.toString();
+
+    return(dateInString);
+}
